5ed9b579593dfef8fb220af4a6636714b3a8e4da
jonathanbalbs <jonathanesh50@gmail.com>
2023-10-30 17:55:07 +0300

    first commit

--- src/main/java/com/balabyeki/interview/SpringApp.java
+++ src/main/java/com/balabyeki/interview/SpringApp.java
@@ -1,16 +1,15 @@
-package com.alibou.security;
+package com.balabyeki.interview;
 
-import com.alibou.security.auth.AuthenticationService;
-import com.alibou.security.auth.RegisterRequest;
-import com.alibou.security.user.Role;
+import com.balabyeki.interview.auth.AuthenticationService;
+import com.balabyeki.interview.auth.RegisterRequest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import static com.alibou.security.user.Role.ADMIN;
-import static com.alibou.security.user.Role.MANAGER;
+import static com.balabyeki.interview.user.Role.ADMIN;
+import static com.balabyeki.interview.user.Role.MANAGER;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
